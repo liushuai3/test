f7ddbaa6d7495acad0d16d7eab6822d8babf7322
liushuai3 <liushuai3@asiainfo.com>
2020-06-03 13:26:17 +0800

    @liushuai3@优化

--- src/cn/lcools/test/third/ProvinceService.java
+++ src/cn/lcools/test/third/ProvinceService.java
@@ -2,12 +2,12 @@ package cn.lcools.test.third;
 
 import java.util.List;
 
-/*
-请完善下面的类
-1.内存越低越好，性能越高越好
-2.线程安全
-3.代码可读性好
-*/
+/**
+ * 请完善下面的类
+ * 1.内存越低越好，性能越高越好
+ * 2.线程安全
+ * 3.代码可读性好
+ */
 public interface ProvinceService {
     String getFormattedJSONByResource(List<AreaResource> areas);
 }
