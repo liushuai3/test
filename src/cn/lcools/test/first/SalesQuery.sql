c3fd48d0e603c471451ef551c9c4fd8fefdfc9c9
liushuai3 <liushuai3@asiainfo.com>
2020-06-03 13:22:52 +0800

    @liushuai3@添加sql

---
+++ src/cn/lcools/test/first/SalesQuery.sql
@@ -0,0 +1,7 @@
+# 1.查询每家店销售额最高的商品情况
+select a.shop, b.item, a.volume
+    from (select shop, max(volume) volume from sales group by shop) a
+    left join sales b on b.volume = a.volume and b.shop = a.shop;
+
+# 2.查询有产品平均销售>200商品的店的平均销售额
+select item, avg(volume) avgVolume from sales group by item having  avgVolume>200;
